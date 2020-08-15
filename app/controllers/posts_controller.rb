379ee492cf59ebecc1608f27dfb3bd3d5b64fb97
arbrown110 <arbrown110@gmail.com>
2020-08-15 11:37:28 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @author = @post.author
   end
 
   def new
@@ -20,6 +21,18 @@ class PostsController < ApplicationController
     @post.save
     redirect_to post_path(@post)
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(title: params[:title], description: params[:description])
+    redirect_to post_path(@post)
+  end
+
+
 
   def edit
     @post = Post.find(params[:id])
